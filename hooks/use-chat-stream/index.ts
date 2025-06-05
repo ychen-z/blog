08f98de0085d48df15c8c750e8688ab491fa7afa
itiswell <52913031+ychen-z@users.noreply.github.com>
2025-06-05 16:27:17 +0800

    misc: use-chat-stream

--- hooks/use-chat-stream/index.ts
+++ hooks/use-chat-stream/index.ts
@@ -1,21 +1,15 @@
-// use
-
-//  const { chatAnswer } = useChatStream(id, {
-//     loading,
-//     onModifyChatItem,
-//     onChangeLoading,
-//     defaultChatAnswer: preAnswer,
-//   });
-
 import { useEffect } from 'react';
 import { useSafeState } from 'ahooks';
+import { EventEmitter } from 'ahooks/es/useEventEmitter';
+import { App } from 'antd';
 import dayjs from 'dayjs';
 import { AiChatMessage } from '@/api/aigc';
 import Typewriter from './typewriter';
 
+const TIMEOUT = 1000 * 60 * 5;
+
 function getSseUrl(id: number) {
-  const prefix = process.env.NODE_ENV === 'development' ? '/test' : '';
-  return `${prefix}/cloud/ai-server/ai/sse/question/send?historyId=${id}`;
+  return `/cloud/ai-server/ai/sse/question/send?historyId=${id}`;
 }
 
 function useChatStream(
@@ -25,10 +19,16 @@ function useChatStream(
     defaultChatAnswer?: string;
     onChangeLoading?: (loading: boolean) => void;
     onModifyChatItem: (id: number, info: Partial<AiChatMessage>) => void;
+    eventEmitter: EventEmitter<string>;
   },
 ) {
-  const { loading, onChangeLoading, defaultChatAnswer, onModifyChatItem } =
-    options;
+  const {
+    loading,
+    onChangeLoading,
+    defaultChatAnswer,
+    onModifyChatItem,
+    eventEmitter,
+  } = options;
 
   const [chatAnswer, setChatAnswer] = useSafeState(defaultChatAnswer);
 
@@ -36,12 +36,29 @@ function useChatStream(
     setChatAnswer(defaultChatAnswer);
   }, [defaultChatAnswer]);
 
+  const { message } = App.useApp();
+
   useEffect(() => {
     if (!loading || !!chatAnswer) return;
     const typeWriter = new Typewriter((str: string) => {
       setChatAnswer((prev) => (prev || '') + str);
+      eventEmitter?.emit('typingAnswer');
     });
     const eventSource = new EventSource(getSseUrl(historyId));
+    const finishTyping = () => {
+      eventSource.close();
+      typeWriter.done();
+      onModifyChatItem(historyId, {
+        chatAnswer,
+        chatAnswerCreateTime: dayjs().format('YYYY-MM-DD hh:mm:ss'),
+      });
+      onChangeLoading?.(false);
+    };
+    // 超时处理
+    const timer = setTimeout(() => {
+      message.error('诊断失败，请缩小部门范围或稍后再试');
+      finishTyping();
+    }, TIMEOUT);
 
     eventSource.onmessage = (event) => {
       try {
@@ -57,38 +74,19 @@ function useChatStream(
       // 处理数据
     };
 
+    eventSource.addEventListener('errorMessage', () => {
+      message.error('诊断失败，请缩小部门范围或稍后再试');
+    });
+
     eventSource.onerror = () => {
-      // 处理错误
-      typeWriter.done();
-      eventSource.close();
-      onModifyChatItem(historyId, {
-        chatAnswer,
-        chatAnswerCreateTime: dayjs().format('YYYY-MM-DD hh:mm:ss'),
-      });
-      onChangeLoading?.(false);
+      finishTyping();
+      clearTimeout(timer);
     };
-    // aigcApi
-    //   .getChatAnswerById({ historyId }, (e) => {
-    //     const nextContent = e?.event?.target?.response;
-    //     console.log('onDownloadProgress:', e, nextContent);
-    //     typeWriter.add(nextContent);
-    //   })
-    //   .then((res) => {
-    //     console.log('>>>>>>>', res);
-    //     setChatAnswer(res?.data || '');
-    //     if (res.success) {
-    //       typeWriter.done();
-    //       onModifyChatItem(historyId, {
-    //         chatAnswer: res?.data,
-    //         chatAnswerCreateTime: dayjs().format('YYYY-MM-DD hh:mm:ss'),
-    //       });
-    //     }
-    //   })
-    //   .finally(() => onChangeLoading?.(false));
     return () => {
       eventSource.close();
       typeWriter.done();
-    }
+      clearTimeout(timer);
+    };
   }, [historyId, loading]);
 
   return {
